bbe817ae6726758f52af3ea94f4425d2536c6115
Flairs13 <flairs13@gmail.com>
2020-10-02 18:11:15 +0300

    Updates

---
+++ precache-manifest.3ba25631a764c152b76d421f99f29390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b4de192ab2c6435b3c479e2e902cc0",
+    "url": "/master-calc/index.html"
+  },
+  {
+    "revision": "1cf3bb3d0335a97e4cf3",
+    "url": "/master-calc/static/css/main.7de3364f.chunk.css"
+  },
+  {
+    "revision": "d255fb33cb1495357242",
+    "url": "/master-calc/static/js/2.6fd11cce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/master-calc/static/js/2.6fd11cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf3bb3d0335a97e4cf3",
+    "url": "/master-calc/static/js/main.da77e97f.chunk.js"
+  },
+  {
+    "revision": "f5b1715e1f45640a0e6d",
+    "url": "/master-calc/static/js/runtime-main.c904ef16.js"
+  }
+]);
